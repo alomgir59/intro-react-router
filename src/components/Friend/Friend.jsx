8124e7ccea102825ade77038ce839791a6320ccb
alomgir59 <alomgirn90@gmail.com>
2023-04-05 21:31:58 +0600

    Dynamic data load & button create, something add & change

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./friend.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Friend = ({ friend }) => {
   // console.log(friend);
   const { name, id, phone, email } = friend;
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate(`/post/${id}`);
+  };
   return (
     <div className="friend">
       <h2>{name}</h2>
@@ -13,6 +17,13 @@ const Friend = ({ friend }) => {
 
       <p>
         <Link to={`/friend/${id}`}>Show me details</Link>
+        <Link to={`/friend/${id}`}>
+          <button className="btn">Show me details</button>
+          <button className="handleBtn" onClick={handleNavigation}>
+            {" "}
+            Button Handler
+          </button>
+        </Link>
       </p>
     </div>
   );
